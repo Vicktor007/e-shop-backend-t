c347ec5b42e71f8f8b01b9c0865ff537fbd78b35
Vicktor007 <vicktord007@gmail.com>
2024-01-04 16:37:55 +0100

    save

--- models/orderModel.js
+++ models/orderModel.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 const orderSchema = mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
-    productUser: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
     orderItems: [
       {
         name: { type: String, required: true },
