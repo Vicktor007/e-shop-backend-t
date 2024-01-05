c7fcd2d101dbf68a5eab4686a6074301bf0dfc1e
Vicktor007 <vicktord007@gmail.com>
2024-01-05 10:21:58 +0100

    save

--- index.js
+++ index.js
@@ -23,10 +23,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({
-    origin: ["http://localhost:5173", "https://e-shop-frontend-t.vercel.app"],
-    credentials: true
-  }));
+app.use(cors());
 
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
