a3d3ca88cf371af4a34302d80f240e6346a20830
Vicktor007 <vicktord007@gmail.com>
2024-01-05 10:17:42 +0100

    save

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -23,32 +23,6 @@ const authenticate = asyncHandler(async (req, res, next) => {
   }
 });
 
-// const authenticate = asyncHandler(async(req, res, next) => {
-//   try {
-//       // check if login token exist
-//       const token = req.cookies.token
-//       if(!token) {
-//           res.status(401) 
-//           throw new Error("Not authorized, please login")      
-//          }
-
-//       //    verify token
-//       const verifiedToken = jwt.verify(token, process.env.JWT_SECRET)
-//       // get user id from token
-//       const user = await User.findById(verifiedToken.id).select("-password")
-//       // check if user exist
-//       if(!user) {
-//           res.status(401)
-//       throw new Error("User not found");
-  
-//       }
-//       req.user = user
-//       next()
-//   } catch (error) {
-//       res.status(401) 
-//       throw new Error("Not authorized, please login")  
-//   }
-// })
 
 const authorizeAdmin = (req, res, next) => {
   if (req.user && req.user.isAdmin) {
